eb662e09e3f8c6d49320535d200e3f73d206d630
MadCow234 <MadCow234@gmail.com>
2019-11-02 01:16:09 -0500

    Add "{user} is typing" functionality

--- index.js
+++ index.js
@@ -2,23 +2,36 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var currentlyTypingList = new Array();
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connect', function(socket) {
   socket.broadcast.emit('connections', 'A user connected.');
-});
 
-io.on('connection', function(socket) {
   socket.on('disconnect', function() {
     socket.broadcast.emit('connections', 'A user disconnected.');
   });
-});
 
-io.on('connection', function(socket) {
   socket.on('messages', function(data) {
     socket.broadcast.emit('messages', data);
+  });
+
+  socket.on('started_typing', function(username) {
+    if (!currentlyTypingList.includes(username)) {
+      currentlyTypingList.push(username);
+      io.emit('typing_status', currentlyTypingList);
+    }
+  });
+
+  socket.on('stopped_typing', function(username) {
+    if (currentlyTypingList.includes(username)) {
+      var index = currentlyTypingList.indexOf(username);
+      currentlyTypingList.splice(index, 1);
+      io.emit('typing_status', currentlyTypingList);
+    }
   });
 });
 
